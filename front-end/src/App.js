0057f675e8e291f3b7709c1eaf5d22711c7d8bbd
DouglasMacKrell <douglasmackrell@pursuit.org>
2022-05-15 10:11:34 -0400

    added express server within frontend folder, axios calls successful

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [prompt, setPrompt] = useState('');
+  const [engine, setEngine] = useState('engine1')
+  const [aiResponse, setAiResponse] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userPrompt = { prompt: prompt, engine: engine };
+    console.log(userPrompt)
+    // fetch('http://localhost:3001/prompt', {
+    //   method: 'POST',
+    //   body: JSON.stringify(userPrompt)
+    // })
+    //   .then((response) => {
+    //     console.log(response)
+    //   })
+    axios.post('http://localhost:3001/prompt', userPrompt)
+      .then((response) => {
+        console.log(response.data.choices)
+        console.log(response.data.choices[0].text)
+        setAiResponse(response.data.choices[0].text)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  // useEffect(() => {
+  //   const askQuestion = (query) => {
+
+  //   }
+  // })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="create">
+      <form onSubmit={handleSubmit}>
+        <label>Enter prompt</label>
+        <textarea
+          required
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        ></textarea>
+        <select
+          value={engine}
+          onChange={(e) => setEngine(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="engine1">Engine 1</option>
+          <option value="engine2">Engine 2</option>
+        </select>
+        <button>Submit</button>
+      </form>
+      <p>{aiResponse}</p>
     </div>
   );
 }
